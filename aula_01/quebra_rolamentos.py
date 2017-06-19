c462c4bdf2ceab9261315ccc067cce85c9917fa2
Alessandrø <ale-remo@hotmail.com>
2017-06-19 15:03:37 -0300

    Add files via upload

--- aula_01/quebra_rolamentos.py
+++ aula_01/quebra_rolamentos.py
@@ -1,20 +1,7 @@
 from random import randint
 
-probabilities = {
-  1000: 0.10,
-  1100: 0.13,
-  1200: 0.25,
-  1300: 0.13,
-  1400: 0.09,
-  1500: 0.12,
-  1600: 0.02,
-  1700: 0.06,
-  1800: 0.05,
-  1900: 0.05
-}
-
-def get_rand_value_from_probabilities(probs):
-  if sum(probs.values()) != 1:
+def get_rand_value_from_probability_table(probs):
+  if not (0.999 <= sum(probs.values()) <= 1.00001):
     print('Invalid table')
     return
   
@@ -32,13 +19,40 @@ def get_rand_value_from_probabilities(probs):
     if rand <= upper_limits[key]:
       return key
   
-vida = 0
-n = 0
+tabela_vida_rolamento = {
+  1000: 0.10,
+  1100: 0.13,
+  1200: 0.25,
+  1300: 0.13,
+  1400: 0.09,
+  1500: 0.12,
+  1600: 0.02,
+  1700: 0.06,
+  1800: 0.05,
+  1900: 0.05
+}
+
+tabela_tempo_espera = {
+  5: 0.6,
+  10: 0.3,
+  15: 0.1
+}
+
+vida_acumulada = 0
+num_sequencia = 1
+tempo_espera_total = 0
+
+while vida_acumulada < 20000:
+  vida_rolamento = get_rand_value_from_probability_table(tabela_vida_rolamento)
+  tempo_espera = get_rand_value_from_probability_table(tabela_tempo_espera)
   
-for i in range(0, 100000):
-  vida += get_rand_value_from_probabilities(probabilities)
-  n += 1
+  print(num_sequencia)
+  print(vida_rolamento)
+  print('--------------')
   
-print(vida / n)
-  
+  vida_acumulada += vida_rolamento
+  tempo_espera_total += tempo_espera
+  num_sequencia += 1
+
+print(tempo_espera_total)
   
